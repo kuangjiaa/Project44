55acc60aeb09548a2bfd304065a95c75204ca7db
kuangjiaa <3073564854@qq.com>
2023-10-21 19:20:52 +0800

    添加项目文件。

---
+++ project44.1.c
@@ -0,0 +1,78 @@
+/*#include<stdio.h>
+#define  MM   34789047
+int main()
+{
+	for (int a= 0; a <= 9; a++) {
+		for (int b = 0; b <= 9; b++) {
+			for (int c = 0; c <= 9; c++) {
+				for (int d = 0; d < +9; d++) {
+					for (int e = 0; e <= 0; e++) {
+						for (int f = 0; f <= 9; f++) {
+							for (int g = 0; g <= 9; g++) {
+								for (int i = 0; i <= 9; i++) {
+									if (a==3&& b == 4 && c == 7 && d == 8 && e == 9 && f == 0 && e == 4 && i == 7) {
+										printf("%d%d%d%d%d%d%d%d", a, b, c, d, e, f, g, i);
+									}
+									else {
+										;
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+
+
+	return 0;
+}*/
+/*#include<stdio.h>
+int isprimer(int m) {
+	int i;
+	for (i = 2; i <= m; i++) {
+		if (m % i == 0) {
+			break;
+		}
+	}
+	if (i == m) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+int main()
+{
+	int isprimer(int m);
+	int n;
+	scanf_s("%d", &n);
+	for (int c = 100; c <= n; c++) {
+		if (isprimer(c)) {
+			printf("%d\n", c);
+		}
+	}
+	return 0;
+}*/
+#include<stdio.h>
+/*int main()
+{
+	int a;
+	scanf_s("%d", &a);
+	for (int i = 2; i <= a; i++) {
+		while (a % i == 0) {
+			printf("%d\n", i);
+			a = a / i;//
+		}
+	}
+	return 0;
+
+}*/
+#include<stdio.h>
+int main()
+{
+
+
+}
